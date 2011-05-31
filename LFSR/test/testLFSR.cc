1872a3848c71cf3f17b5e8f98ea46c2cf259993b
Yves Stadler <mvyonline@gmail.com>
2011-05-31 17:33:42 +0200

    Correction of seed. Test should run ok now

--- LFSR/test/testLFSR.cc
+++ LFSR/test/testLFSR.cc
@@ -1,8 +1,8 @@
-#include "LFSR.hh"
+#include "../src/LFSR.hh"
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    LFSR* l = new LFSR(TAP32 | TAP7 | TAP5 | TAP2 | TAP1 , 0x6C9AC645);
+    LFSR* l = new LFSR(TAP32 | TAP7 | TAP5 | TAP3 | TAP2 | TAP1 , 0x6C9AC645);
 
     for(int i = 0; i < 100; i++) {
         std::cout << l->getValue() << " ";
