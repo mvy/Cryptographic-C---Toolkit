9c492199f026a31236e9b9c61c9543302892a005
Yves Stadler <mvyonline@gmail.com>
2011-05-31 16:13:39 +0200

    Testing main executable

---
+++ LFSR/testLFSR.cc
@@ -0,0 +1,14 @@
+#include "LFSR.hh"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    LFSR* l = new LFSR(TAP32 | TAP7 | TAP5 | TAP2 | TAP1 , 0x6C9AC645);
+
+    for(int i = 0; i < 100; i++) {
+        std::cout << l->getValue() << " ";
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "1 0 1 0 0 0 1 0 0 1 1 0 0 0 1 1 0 1 0 1 1 0 0 1 0 0 1 1 0 1 1 0 0 0 1 0 0 0 1 0 1 0 0 0 0 0 1 1 0 1 0 1 0 0 1 0 1 0 1 0 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 1 0 1 1 1 0 1 0 1 0 0 0 0 1 1 0 0 0 0 0 0 0 1 0 0" << std::endl;
+}
